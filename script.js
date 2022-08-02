694613c143e3fb8edd2a288a81a93eb4c6c895b6
Matt Charles <matthewcharles90@gmail.com>
2022-08-02 15:20:36 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+let userInput = document.getElementById("user-input").value;
+
+fetch(url + "pizza")
+    .then((response) => response.json())
+    .then((data) => {
+        let myMeal = data.meals[0]
+        console.log(myMeal)
+        console.log(myMeal.strMealThumb)
+        console.log(myMeal.strMeal)
+        console.log(myMeal.strArea)
+        console.log(myMeal.strInstructions)
+        let count = 1;
+        let ingredients = [];
+        for (let i in myMeal) {
+            let ingredient = "";
+            let measure = "";
+            if (i.startsWith("strIngredient") && myMeal
+            [i]) {
+                ingredient = myMeal[i];
+                measure = myMeal[`strMeasure` + count];
+                count += 1;
+                ingredients.push(`${measure} ${ingredient}
+                `)
+            }
+        }
+
+        console.log(ingredients);
+
+        result.innerHTML = `
+        <img src= ${myMeal.strMealThumb}>
+        <div class="details">
+           <h2>${myMeal.strMeal}</h2>
+           <h2>${myMeal.strArea}</h2>
+        </div>
+
+        <div id="ingredient-con"></div>
+        <div id="recipe">
+            <button id="hide-recipe">X</button>
+            <pre id="instructions">${myMeal.strInstructions}</pre>
+        </div>
+        `;
+
+    });
